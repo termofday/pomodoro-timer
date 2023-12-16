c29b1cb9917b6ea0f72013d0a5b7216f39b89ba6
termofday <29450358+termofday@users.noreply.github.com.>
2023-12-16 21:38:06 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      
+      
+
+        /*
+
+        - break session not <= 0 or > 60
+        - If the timer is running, the element with the id of time-left should display 
+        the remaining time in mm:ss format 
+        (decrementing by a value of 1 and updating the display every 1000ms).
+
+        */
+      
+
+
+        <div>
+        
+        <div id="break-label">
+
+        </div>
+
+        <div id="session-label">
+
+        </div>
+
+        <div id="break-decrement"></div>
+
+        <div id="session-decrement"></div>
+
+        <div id="break-increment"></div>
+
+        <div id="session-increment"></div>
+
+        <div id="break-length"></div>
+
+        <div id="sesseion-length"></div>
+
+        <div id="timer-label"></div>
+
+        <div id="time-left">mm:ss</div>
+
+        <div id="start_stop"></div>
+    
+        <div id="reset"></div>
+
+        </div>
+      
+    
   );
 }
-
-export default App;
